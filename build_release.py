562882c504a910de0fc102ed9abedebdd4f31fa2
Ilya Voronin <ivoronin@gmail.com>
2018-05-01 19:45:00 +0300

    Switched to latest create-dmg version

--- build_release.py
+++ build_release.py
@@ -7,11 +7,9 @@ PROJECT='ArchiveMounter.xcodeproj'
 SCHEME='ArchiveMounter'
 CONFIGURATION='Release'
 ARCHIVEPATH = f'ArchiveMounter.xcarchive'
-IDENTITY="Mac Developer"
 
 if __name__ == '__main__':
     rmrf(ARCHIVEPATH)
     run(f'xcodebuild -project "{PROJECT}" -scheme "{SCHEME}" \
         -configuration "{CONFIGURATION}" -archivePath "{ARCHIVEPATH}" archive')
-    run(f'./node_modules/.bin/create-dmg --overwrite --identity "{IDENTITY}" \
-        "{ARCHIVEPATH}/Products/Applications/Archive Mounter.app"')
+    run(f'./node_modules/.bin/create-dmg --overwrite "{ARCHIVEPATH}/Products/Applications/Archive Mounter.app"')
